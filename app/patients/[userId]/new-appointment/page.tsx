8768e5162867c9bf3035b2e1554f6e412041e71d
Lesego Ratau <rataulesego95@gmail.com>
2025-05-27 21:47:30 +0200

    Appointment Form database

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,12 +1,17 @@
+import AppointmentForm from "@/components/forms/AppointmentForm";
 import PatientForm from "@/components/forms/PatientForm";
+import { getPatient } from "@/lib/actions/patient.actions";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function NewAppointment() {
+export default async function NewAppointment({
+  params: { userId },
+}: SearchParamProps) {
+  const patient = await getPatient(userId);
   return (
     <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-contaner max-w-[860px] flex-1 justify-between">
+      <section className="remove-scrollbar container">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
           <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
@@ -15,12 +20,13 @@ export default function NewAppointment() {
             className="mb-12 h-10 w-fit"
           />
 
-          {/* <PatientForm /> */}
+          <AppointmentForm
+            type="create"
+            userId={userId}
+            patientId={patient.$id}
+          />
 
-          <p className="justify-items-end text-dark-600 xl:text-left">
-            {" "}
-            © 2025 CarePulse
-          </p>
+          <p className="copyright mt-10 py-12"> © 2025 CarePulse</p>
         </div>
       </section>
       <Image
